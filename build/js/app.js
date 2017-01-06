69672457f2b4d923a0229253bcb346f6b6c225b6
Ezra Sandzer-Bell <Equilibriumarts@gmail.com>
2017-01-06 09:53:16 -0800

    get json file and access results in console log, but does not print text to page

--- build/js/app.js
+++ build/js/app.js
@@ -19,27 +19,20 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  exports.getDoctors = function(medicalIssue) {
+
+  $('#issueForm').submit(function(event){
+    event.preventDefault();
+    medicalIssue = $("#issue").val();
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
      .then(function(result) {
-        findSolution(result);
-        console.log(result);
+        var returned = result.data[0].practices[0].name
+        $('doctorList').text(returned);
+        console.log(result.data[0].practices[0].name);
       })
      .fail(function(error){
         console.log("fail");
-      });
-  };
-
-  function findSolution(theIssue){
-
-    var doctors = theIssue;
-
-    $('#issueForm').submit(function(event){
-      event.preventDefault();
-      medicalIssue = $("#issue").val();
-      $(".doctorList").text(doctors);
     });
-  }
+  });
 });
 
 },{"./../.env":1,"./../js/doctor.js":2}]},{},[3]);
