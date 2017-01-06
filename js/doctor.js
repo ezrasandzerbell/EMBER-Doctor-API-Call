05d53ff56a42240642187cad53af531453dd3cc0
Ezra Sandzer-Bell <Equilibriumarts@gmail.com>
2017-01-06 11:33:25 -0800

    fill out forEach loop to run on result.data with resultArray as argument. Run resultArray.practices.name and assign to variable. Push to empty array outside the function (allDocNames)

--- js/doctor.js
+++ js/doctor.js
@@ -11,14 +11,19 @@ Doctor.prototype.names = function(apiResults) {
 };
 
 exports.getDoctors = function(medicalIssue) {
+
+  var allDocNames = [];
+
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      result.data.forEach(function()
+      result.data.forEach(function(resultArray){
+        var docName = resultArray.practices.name;
+        allDocNames.push(docName);
+      });
 
-      )
       var doctors = new Doctor();
-      doctors.list();
-      $('doctorList').text()
+      finalList = doctors.list();
+      $('doctorList').text(finalList)
     })
    .fail(function(error){
       console.log("fail");
