e777e5c89a48e90cc9572946f3b6266476b4194a
Ezra Sandzer-Bell <Equilibriumarts@gmail.com>
2017-01-06 09:23:15 -0800

    set up API key and match variable names with get retrieval. not currently working

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "c2d7269116a1f356e5f16b448c219c73";
+
+},{}],2:[function(require,module,exports){
 function Doctor() {
   this.word = "";
 }
@@ -11,26 +14,32 @@ function Doctor() {
 
 exports.doctorModule = Doctor;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 var Doctor = require('./../js/doctor.js').doctorModule;
-
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   exports.getDoctors = function(medicalIssue) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
      .then(function(result) {
+        findSolution(result);
         console.log(result);
       })
      .fail(function(error){
         console.log("fail");
       });
   };
-  $('#issueForm').submit(function(event){
-    event.preventDefault();
-    submission = $("#issue").val();
-    $(".output").text(submission);
-  });
 
+  function findSolution(theIssue){
+
+    var doctors = theIssue;
+
+    $('#issueForm').submit(function(event){
+      event.preventDefault();
+      medicalIssue = $("#issue").val();
+      $(".doctorList").text(doctors);
+    });
+  }
 });
 
-},{"./../js/doctor.js":1}]},{},[2]);
+},{"./../.env":1,"./../js/doctor.js":2}]},{},[3]);
