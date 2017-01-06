972353832eaad1d75fdb6817536002a1b8fa96a6
Ezra Sandzer-Bell <Equilibriumarts@gmail.com>
2017-01-06 08:44:27 -0800

    add API key to doctor interface

--- build/js/app.js
+++ build/js/app.js
@@ -16,7 +16,15 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 
 $(document).ready(function() {
-
+  exports.getDoctors = function(medicalIssue) {
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+     .then(function(result) {
+        console.log(result);
+      })
+     .fail(function(error){
+        console.log("fail");
+      });
+  };
   $('#issueForm').submit(function(event){
     event.preventDefault();
     submission = $("#issue").val();
