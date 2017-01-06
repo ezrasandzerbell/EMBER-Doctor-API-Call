03f4e0074d27b05847a4fe1fc9e3454d9c05e9de
Ezra Sandzer-Bell <Equilibriumarts@gmail.com>
2017-01-06 08:40:12 -0800

    add header tags, create functional jquery form retrieval code on backend to pass user submission through and display input in a div class on index.html page

--- build/js/app.js
+++ build/js/app.js
@@ -16,7 +16,13 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 
 $(document).ready(function() {
-  
+
+  $('#issueForm').submit(function(event){
+    event.preventDefault();
+    submission = $("#issue").val();
+    $(".output").text(submission);
+  });
+
 });
 
 },{"./../js/doctor.js":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -2,5 +2,11 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 
 $(document).ready(function() {
-  
+
+  $('#issueForm').submit(function(event){
+    event.preventDefault();
+    submission = $("#issue").val();
+    $(".output").text(submission);
+  });
+
 });
